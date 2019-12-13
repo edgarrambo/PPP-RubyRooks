830197d2c44a48cdf38981df2c17558741f8aa61
michloud <chinoah96@gmail.com>
2019-12-13 16:00:30 +0000

    Def CRUD methods in controller, Add forms to new page, add test object to db

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,24 @@
 class GamesController < ApplicationController
 
   def new
-
+    @game = Game.new
   end
 
   def create
-
+    @game = Game.create(game_params)
+    redirect_to game_path(@game)
   end
 
 
   def show
+    @game = Game.find(params[:id])
+  end
 
+
+  private
+
+
+  def game_params
+    params.require(:game).permit(:name)
   end
 end
