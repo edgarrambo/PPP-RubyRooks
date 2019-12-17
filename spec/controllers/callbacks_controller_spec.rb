97b14d894343fb268c2c46a88a2a98fb3611edef
Shane Quick <shanesfast@gmail.com>
2019-12-16 20:15:14 -0600

    Change CallbacksController tests

--- spec/controllers/callbacks_controller_spec.rb
+++ spec/controllers/callbacks_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CallbacksController, type: :controller do
     OmniAuth.config.test_mode = false
   end
 
-  context 'omniauth-facebook' do
+  describe 'Omniauth' do
     before(:each) do
       OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
         provider: 'facebook',
@@ -24,25 +24,7 @@ RSpec.describe CallbacksController, type: :controller do
       sign_in @user
     end
 
-    it 'checks if Omniauth-facebook is setting an email' do
-      expect(@user.email).to eq(User.last.email)
-    end
-  end
-
-  context 'omniauth-twitter' do
-    before(:each) do
-      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
-        provider: 'twitter',
-        uid: '123545',
-        info: { email: 'none@none.com' }
-      )
-      Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
-      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
-      @user = User.from_omniauth(Rails.application.env_config['omniauth.auth'])
-      sign_in @user
-    end
-
-    it 'checks if Omniauth-twitter is setting an email' do
+    it 'checks if Omniauth hash is setting an email with devise' do
       expect(@user.email).to eq(User.last.email)
     end
   end
