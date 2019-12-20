32f06fb42f38d19997bbcb70099d947f46a6da98
Eurus Wagner <euruswagner@gmail.com>
2019-12-20 16:22:41 +0000

    Board is done

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@
 class Game < ApplicationRecord
   has_one :creating_user, :class_name => 'User'
   has_one :invited_user, :class_name => 'User'
-  has_one :first_move, :class_name => 'User'
+  belongs_to :first_move, :class_name => 'User', :foreign_key => 'first_move_id'
   has_one :winner, :class_name => 'User'
   has_many :moves
   has_many :comments
