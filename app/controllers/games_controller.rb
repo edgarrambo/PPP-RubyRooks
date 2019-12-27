0978c0753de4badbca5b1d6778ca9d6514b78999
Eurus Wagner <euruswagner@gmail.com>
2019-12-27 16:17:57 +0000

    Controller works/Tests do not

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.create(game_params.merge(creating_user: current_user))
     if @game.valid?
+      populate_game
       redirect_to game_path(@game)
     else
       render :new, status: :unprocessable_entity
@@ -39,5 +40,42 @@ class GamesController < ApplicationController
 
   def game_params
     params.require(:game).permit(:name)
+  end
+
+  def populate_game
+    # White Rooks
+    @game.pieces.create(x_position: 0, y_position: 0, piece: 0)
+    @game.pieces.create(x_position: 0, y_position: 7, piece: 0)
+    # White Knights
+    @game.pieces.create(x_position: 0, y_position: 1, piece: 1)
+    @game.pieces.create(x_position: 0, y_position: 6, piece: 1)
+    # White Bishops
+    @game.pieces.create(x_position: 0, y_position: 2, piece: 2)
+    @game.pieces.create(x_position: 0, y_position: 5, piece: 2)
+    # White Queen
+    @game.pieces.create(x_position: 0, y_position: 3, piece: 3)
+    # White King
+    @game.pieces.create(x_position: 0, y_position: 4, piece: 4)
+    # White Pawns
+    8.times do |y|
+      @game.pieces.create(x_position: 1, y_position: y, piece: 5)
+    end
+    # Black Rooks
+    @game.pieces.create(x_position: 7, y_position: 0, piece: 6)
+    @game.pieces.create(x_position: 7, y_position: 7, piece: 6)
+    # Black Knights
+    @game.pieces.create(x_position: 7, y_position: 1, piece: 7)
+    @game.pieces.create(x_position: 7, y_position: 6, piece: 7)
+    # Black Bishops
+    @game.pieces.create(x_position: 7, y_position: 2, piece: 8)
+    @game.pieces.create(x_position: 7, y_position: 5, piece: 8)
+    # Black Queen
+    @game.pieces.create(x_position: 7, y_position: 3, piece: 9)
+    # Black King
+    @game.pieces.create(x_position: 7, y_position: 4, piece: 10)
+    # Black Pawns
+    8.times do |y|
+      @game.pieces.create(x_position: 6, y_position: y, piece: 11)
+    end
   end
 end
