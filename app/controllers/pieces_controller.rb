811437d3170b43291bdcf3cfddb2510daeef7576
michloud <chinoah96@gmail.com>
2019-12-29 20:09:06 +0000

    add links to pieces

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,14 @@
 class PiecesController < ApplicationController
+
+  def show
+
+
+  end
+
+
+  def update
+    @piece.update_attributes(piece_params)
+    redirect_to game_path(@game)
+
+  end
 end
