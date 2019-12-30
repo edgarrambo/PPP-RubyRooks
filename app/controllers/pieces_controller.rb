5f5d8957bfc3056c36a6efc80f013fc80fb5686b
michloud <chinoah96@gmail.com>
2019-12-30 20:04:25 +0000

    Make game board slightly larger; add user permission to piece controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,11 +1,10 @@
 class PiecesController < ApplicationController
-
+  before_action :authenticate_user!
+  #before_action :require_authorized_to_move_piece
+  
   def show
     @piece = Piece.find(params[:id])
-
-
   end
-
 
   def update_position
     @piece = Piece.find(params[:piece_id])
@@ -19,4 +18,11 @@ class PiecesController < ApplicationController
   def piece_params
     params.permit(:x_position, :y_position)
   end
+
+  #def require_authorized_to_move_piece
+    #if current_user != creating_user || current_user != invited_user
+        #render plain: 'Game in progress', status: :Unauthorized
+    #end
+  #end
+
 end
