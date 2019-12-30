3d7acec674a780dfd210e32c051f51e576eaaee5
Eurus Wagner <euruswagner@gmail.com>
2019-12-30 15:57:46 +0000

    Links to updates on pieces show page

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,14 +2,21 @@ class PiecesController < ApplicationController
 
   def show
     @piece = Piece.find(params[:id])
-    
+
 
   end
 
 
-  #def update
-    #@piece.update_attributes(piece_params)
-    #redirect_to game_path(@game)
+  def update_position
+    @piece = Piece.find(params[:piece_id])
+    @piece.update_attributes(x_position: x, y_position: y_position)
+    redirect_to game_path(@game)
 
-  #end
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:x_position, :y_position)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   root 'games#index'
-  resources :pieces, only: [:show, :update]
+  resources :pieces, only: [:show] do
+    get :update_position
+  end
+
   resources :games, only: %i[index new create show] do
     get :update_invited_user
   end
