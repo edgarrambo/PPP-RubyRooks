69a24b7b74d54f2c5df847626276dadfc55997f8
Eurus Wagner <euruswagner@gmail.com>
2019-12-31 16:05:27 +0000

    Authentication for player_one and player_two

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,5 +1,6 @@
 class PiecesController < ApplicationController
   before_action :authenticate_user!
+  before_action :player_one_can_only_move_white_and_player_two_can_only_move_black, only: [:show, :update]
 
   def show
     @piece = Piece.find(params[:id])
@@ -16,5 +17,12 @@ class PiecesController < ApplicationController
 
   def piece_params
     params.permit(:x_position, :y_position)
+  end
+
+  def player_one_can_only_move_white_and_player_two_can_only_move_black
+    @piece = Piece.find(params[:id])
+    if @piece.piece < 6 && @piece.game.player_one != current_user || @piece.piece > 5 && @piece.game.player_two != current_user
+      redirect_to game_path(@piece.game), alert: "That is not your piece!"
+    end
   end
 end
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
+  describe 'pieces#update action' do
+    it 'updates a pieces location' do
+      user = create(:user)
+      sign_in user
 
+      game = create(:game)
+      piece = game.pieces.last
+
+      patch :update, params: {
+        id: piece.id,
+        piece: {
+          x_position: 4,
+          y_position: 7
+        }
+      }
+
+      expect(response).to redirect_to game_path(game.id)
+      expect(piece.x_position).to eq 4
+      expect(piece.y_position).to eq 7
+    end
+  end
 end
