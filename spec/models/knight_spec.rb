8437254e14b56493ad71136fe4a64217e73ffe47
Shane Quick <shanesfast@gmail.com>
2020-01-02 14:08:52 -0600

    Add specs for invalid moves, remove unnecessary before block.

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -2,51 +2,73 @@ require 'rails_helper'
 
 RSpec.describe Knight, type: :model do
   describe 'valid_move?' do
-    before(:each) do
-      @user = create(:user)
-      sign_in @user
+    context 'valid moves' do
+      it 'allows a vertical move 2 up and 1 right' do
+        knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(1, 3)).to eq true
+      end
 
-      @game = create(:game)
+      it 'allows a vertical move 2 up and 1 left' do
+        knight = create(:knight, x_position: 1, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(0, 3)).to eq true
+      end
+
+      it 'allows a vertical move 2 down and 1 right' do
+        knight = create(:knight, x_position: 0, y_position: 7, piece_number: 1)
+        expect(knight.valid_move?(1, 5)).to eq true
+      end
+
+      it 'allows a vertical move 2 down and 1 left' do
+        knight = create(:knight, x_position: 1, y_position: 7, piece_number: 1)
+        expect(knight.valid_move?(0, 5)).to eq true
+      end
+
+      it 'allows a horizontal move 2 right and 1 up' do
+        knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(2, 2)).to eq true
+      end
+
+      it 'allows a horizontal move 2 right and 1 down' do
+        knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(2, 0)).to eq true
+      end
+
+      it 'allows a horizontal move 2 left and 1 up' do
+        knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(5, 2)).to eq true
+      end
+
+      it 'allows a horizontal move 2 left and 1 down' do
+        knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(5, 0)).to eq true
+      end
     end
 
-    it 'allows a vertical move 2 up and 1 right' do
-      knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(1, 3)).to eq true
-    end
+    context 'invalid moves' do
+      it 'does not allow a vertical move up 3' do
+        knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(7, 4)).to eq false
+      end
 
-    it 'allows a vertical move 2 up and 1 left' do
-      knight = create(:knight, x_position: 1, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(0, 3)).to eq true
-    end
+      it 'does not allow a vertical down 2' do
+        knight = create(:knight, x_position: 7, y_position: 6, piece_number: 1)
+        expect(knight.valid_move?(7, 4)).to eq false
+      end
 
-    it 'allows a vertical move 2 down and 1 right' do
-      knight = create(:knight, x_position: 0, y_position: 7, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(1, 5)).to eq true
-    end
+      it 'does not allow a horizontal move right 3' do
+        knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(3, 1)).to eq false
+      end
 
-    it 'allows a vertical move 2 down and 1 left' do
-      knight = create(:knight, x_position: 1, y_position: 7, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(0, 5)).to eq true
-    end
+      it 'does not allow a horizontal move left 3' do
+        knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1)
+        expect(knight.valid_move?(4, 1)).to eq false
+      end
 
-    it 'allows a horizontal move 2 right and 1 up' do
-      knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(2, 2)).to eq true
-    end
-
-    it 'allows a horizontal move 2 right and 1 down' do
-      knight = create(:knight, x_position: 0, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(2, 0)).to eq true
-    end
-
-    it 'allows a horizontal move 2 left and 1 up' do
-      knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(5, 2)).to eq true
-    end
-
-    it 'allows a horizontal move 2 left and 1 down' do
-      knight = create(:knight, x_position: 7, y_position: 1, piece_number: 1, game_id: @game.id)
-      expect(knight.valid_move?(5, 0)).to eq true
+      it 'does not allow a diagonal move up and right 3' do
+        knight = create(:knight, x_position: 0, y_position: 0, piece_number: 1)
+        expect(knight.valid_move?(3, 3)).to eq false
+      end
     end
   end
 end
