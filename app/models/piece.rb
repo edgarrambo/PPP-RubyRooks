669dcc77c5bcd4f5ee525eac6c2a27617d9df5ae
Eurus Wagner <euruswagner@gmail.com>
2020-01-03 20:55:57 +0000

    Refactor of piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,45 +2,62 @@
 
 class Piece < ApplicationRecord
   belongs_to :game
+  
+  def is_white?
+    piece_number < 6
+  end
+
+  def is_black?
+    piece_number > 5
+  end
 
   def is_obstructed?(x, y)
     x_sorted_array = [x, x_position].sort
     y_sorted_array = [y, y_position].sort
 
     obstructions = game.pieces.find do |chess_piece|
-      next true if (x - chess_piece.x_position).abs == (y - chess_piece.y_position).abs &&
-                   chess_piece.x_position.between?(x_sorted_array[0] + 1, x_sorted_array[1] - 1) &&
-                   chess_piece.y_position.between?(y_sorted_array[0] + 1, y_sorted_array[1] - 1)
-
+      next true if has_diagonal_obstruction?(x, y, chess_piece, x_sorted_array, y_sorted_array)
       next true if chess_piece.x_position.between?(x_sorted_array[0] + 1, x_sorted_array[1] - 1)
 
       chess_piece.y_position.between?(y_sorted_array[0] + 1, y_sorted_array[1] - 1)
+
     end
 
-    obstructions.present?
+    obstructions.present? || players_own_piece_is_there?(x, y) 
+    
+  end
+  
+  def has_diagonal_obstruction?(x, y, chess_piece, x_sorted_array, y_sorted_array)
+    is_eq_abs = (x - chess_piece.x_position).abs == (y - chess_piece.y_position).abs
+    is_between_x = chess_piece.x_position.between?(x_sorted_array[0] + 1, x_sorted_array[1] - 1)
+    is_between_y = chess_piece.y_position.between?(y_sorted_array[0] + 1, y_sorted_array[1] - 1)
+    return is_eq_abs && is_between_x && is_between_y
+  end
+  
+  def players_own_piece_is_there?(x, y)
+    occupying_piece = Piece.where(x_position: x, y_position: y, game_id: game.id)
+    if occupying_piece.any? then
+      return occupying_piece.first.is_white? && is_white? || occupying_piece.first.is_black? && is_black?
+    else
+      false
+    end
   end
 
-  def move_to!(new_x, new_y)
-    # Check if a piece is at this position
-    if Piece.where(x_position: new_x, y_position: new_y, game_id: game.id)[0]
-      occupying_piece = Piece.where(x_position: new_x, y_position: new_y, game_id: game.id)[0]
+  def move_to!(x,y) 
+    occupying_piece = Piece.where(x_position: x, y_position: y, game_id: game.id)
+    if occupying_piece.any? then
+      occupying_piece.first.set_captured!
+      occupying_piece.first.save
+    end
+    assign_attributes(x_position: x, y_position: y)
+    save
+  end
 
-      # Check if the piece occupying the space is white(0-5) and the moving piece is black(6-11)
-      if occupying_piece.piece_number > 5 && piece_number < 6
-        occupying_piece.update(x_position: 8, y_position: 0)
-        update(x_position: new_x, y_position: new_y)
-
-      # Check if the piece occupying the space is black(6-11) and the moving piece is white(0-5)
-      elsif occupying_piece.piece_number < 6 && piece_number > 5
-        occupying_piece.update(x_position: 9, y_position: 0)
-        update(x_position: new_x, y_position: new_y)
-      else
-        ActionDispatch::Flash.new(alert: 'You cannot move here.')
-      end
-
-    # If no piece in this position, update moving piece position to this position
+  def set_captured! # TODO: Eventually should add a column to table that states whether a piece is captured
+    if is_white?
+      assign_attributes(x_position: 9, y_position: 0)
     else
-      update(x_position: new_x, y_position: new_y)
+      assign_attributes(x_position: 8, y_position: 0)
     end
   end
 end
