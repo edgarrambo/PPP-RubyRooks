25446316e2b3068024d72de62dac56a058568e5c
Shane Quick <shanesfast@gmail.com>
2020-01-03 16:34:31 -0600

    Add migration that was removed by drop_table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_010608) do
+ActiveRecord::Schema.define(version: 2020_01_02_030405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_01_02_010608) do
     t.integer "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.index ["game_id"], name: "index_pieces_on_game_id"
   end
 
