cf849bbe644fb5a7d8c986e344c920873c7a3259
Eurus Wagner <euruswagner@gmail.com>
2020-01-06 17:07:32 +0000

    valid_move? for queen and tests

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -5,6 +5,14 @@ class Queen < Piece
         return false
       end 
 
-      
+      if y - y_position == 0 # Horizontal Check
+        return true
+      elsif x - x_position == 0 # Vertical Check
+        return true
+      elsif (x - x_position).abs == (y - y_position).abs # Diagonal Check
+        return true
+      else
+        return false
+      end
     end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -3,11 +3,55 @@ require 'rails_helper'
 RSpec.describe Queen, type: :model do
   describe 'valid_move? for the queen' do
     before(:each) do
-      @queen = create(:queen, x_position: 3, y_position: 4, piece_number: 5)
+      @queen = create(:queen, x_position: 3, y_position: 4)
     end
 
     it 'allows the queen to move multiple spaces horizontally' do
-      excpect(@queen.valid_move(3,5)).to be true
+      expect(@queen.valid_move?(7,4)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces horizontally' do
+      expect(@queen.valid_move?(0,4)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces vertically' do
+      expect(@queen.valid_move?(3,7)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces vertically' do
+      expect(@queen.valid_move?(3,0)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces diagonally' do
+      expect(@queen.valid_move?(7,0)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces diagonally' do
+      expect(@queen.valid_move?(6,7)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces diagonally' do
+      expect(@queen.valid_move?(0,1)).to be true
+    end
+
+    it 'allows the queen to move multiple spaces diagonally' do
+      expect(@queen.valid_move?(0,7)).to be true
+    end
+
+    it 'does not allow an L shaped move' do
+      expect(@queen.valid_move?(5,3)).to be false
+    end
+
+    it 'does not allow a random move' do
+      expect(@queen.valid_move?(5,7)).to be false
+    end
+
+    it 'checks for obstructions' do
+      game = create(:game)
+      queen = create(:queen, x_position: 3, y_position: 4, game_id: game.id)
+      piece = create(:piece, x_position: 4, y_position: 5, game_id: game.id)
+
+      expect(queen.valid_move?(6,7)).to be false
     end
   end
 end
