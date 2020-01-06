782121d110c0ba41100dd5798e2fd95b09e8e4b1
michloud <54552863+michloud@users.noreply.github.com>
2020-01-06 18:23:55 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2020_01_02_030405) do
     t.index ["game_id"], name: "index_pieces_on_game_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
