9b756cb7bf72c272415e61530d773fd085bfb2b1
michloud <chinoah96@gmail.com>
2020-01-06 20:23:21 +0000

    Complete correct rook model valid move, add tests

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
 
+ describe 'valid_move? for the rook' do
+    before(:each) do
+      @rook = create(:rook, x_position: 2, y_position: 2)
+    end
+    it 'allows the rook to move multiple squares horizontally' do
+      expect(@rook.valid_move?(7,2)).to be true
+    end
+    it 'allows the rook to move multiple squares vertically' do
+      expect(@rook.valid_move?(2,4)).to be true
+    end
+    it 'should not allow the rook to move multiple squares diagonally' do
+      expect(@rook.valid_move?(1,1)).to be false
+    end
+  end
 end
