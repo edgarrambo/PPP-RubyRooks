1cfc4db3eac0490dc50f7ee23b99944aa4759836
Eurus Wagner <euruswagner@gmail.com>
2020-01-06 16:41:06 +0000

    Wrote first test

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,10 @@
 class Queen < Piece
+
+    def valid_move?(x, y)
+      if is_obstructed?(x, y)
+        return false
+      end 
+
+      
+    end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
+  describe 'valid_move? for the queen' do
+    before(:each) do
+      @queen = create(:queen, x_position: 3, y_position: 4, piece_number: 5)
+    end
 
+    it 'allows the queen to move multiple spaces horizontally' do
+      excpect(@queen.valid_move(3,5)).to be true
+    end
+  end
 end
