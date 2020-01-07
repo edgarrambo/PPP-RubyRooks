7001b20b2314e847480a2cc2d91f5fd75b405297
DanielChampagne <54183069+DanielChampagne@users.noreply.github.com>
2020-01-07 12:38:24 -0500

    Update king.rb
    Added if elsif statements

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,8 +1,16 @@
 class King < Piece
 
   def valid_move?(new_x, new_y)
-      x_distance = (x_position - new_x).abs
-      y_distance = (y_position - new_y).abs    
-      x_distance == 1 && y_distance == 1      
+    
+    if (x_position - new_x).abs == 1 && (y_position - new_y).abs == 0
+      return true
+    elsif (x_position - new_x).abs == 0 && (y_position - new_y).abs == 1
+      return true
+    elsif (x_position - new_x).abs == 1 && (y_position - new_y).abs == 1
+      return true
+    else
+      return false
+    end    
+    
   end  
 end
