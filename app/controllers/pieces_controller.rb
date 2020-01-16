5fe00eb54f7166fdca9c89e14261314915402f56
michloud <chinoah96@gmail.com>
2020-01-16 14:03:22 +0000

    update ajax and board partial html

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -8,22 +8,31 @@ class PiecesController < ApplicationController
   end
 
   def update
+    @piece = Piece.find(params[:id])
     x = piece_params[:x_position].to_i
     y = piece_params[:y_position].to_i
-    Piece.find_by_id(params[:id]).move_to!(x, y)
-    redirect_to game_path(@piece.game)
+    if @piece.move_to!(x,y)
+      @piece.update(piece_params)
+    end
+    respond_to do |format|
+      format.html
+      format.json {render json: @piece, status: :ok }
+    end
   end
 
   private
 
   def piece_params
-    params.permit(:x_position, :y_position)
+    params.require(:piece).permit(:id, :x_position, :y_position)
   end
 
   def player_one_can_only_move_white_and_player_two_can_only_move_black
     @piece = Piece.find(params[:id])
     if @piece.is_white? && @piece.game.player_one != current_user || !@piece.is_white? && @piece.game.player_two != current_user
-      redirect_to game_path(@piece.game), alert: "That is not your piece!"
+      respond_to do |format|
+        format.html
+        format.json {render json: @piece.game, alert: "That is not your piece!", status: 422}
+      end
     end
   end
 
@@ -32,7 +41,11 @@ class PiecesController < ApplicationController
     x = piece_params[:x_position].to_i
     y = piece_params[:y_position].to_i
     if !@piece.valid_move?(x, y)
-      redirect_to game_path(@piece.game), alert: "This is not a valid move!"
+      respond_to do |format|
+        format.html {redirect_to game_path(@piece.game), alert: "Invalid move!"}
+        format.json {render json: @piece.game, messsage: "Invalid move!", status: 422}
+      end 
     end
   end
+
 end
