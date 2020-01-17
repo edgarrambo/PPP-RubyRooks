246223eab830ea208262578acf833674408147ef
Shane Quick <shanesfast@gmail.com>
2020-01-17 00:31:56 -0600

    AJAX sort of working now

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -9,13 +9,15 @@ class PiecesController < ApplicationController
 
   def update
     @piece = Piece.find(params[:id])
+    @game = Game.find(@piece.game_id)
     x = piece_params[:x_position].to_i
     y = piece_params[:y_position].to_i
-    @piece.move_to!(x,y)
-      
+    @piece.move_to!(x, y)
+
     respond_to do |format|
       format.html
-      format.json {render json: @piece, status: :ok }
+      format.js { render 'update.js.erb', game: @game }
+      format.json { render json: @piece, status: :ok, file: '/pieces/update' }
     end
   end
 
@@ -27,10 +29,12 @@ class PiecesController < ApplicationController
 
   def player_one_can_only_move_white_and_player_two_can_only_move_black
     @piece = Piece.find(params[:id])
-    if @piece.is_white? && @piece.game.player_one != current_user || !@piece.is_white? && @piece.game.player_two != current_user
-      respond_to do |format|
-        format.html {redirect_to game_path(@piece.game), alert: "That is not your piece!"}
-      end
+    return unless @piece.is_white? && @piece.game.player_one != current_user ||
+                  !@piece.is_white? && @piece.game.player_two != current_user
+
+    respond_to do |format|
+      format.html { flash.alert = 'That is not your piece!' }
+      format.json { render json: @piece, status: 422 }
     end
   end
 
@@ -38,10 +42,11 @@ class PiecesController < ApplicationController
     @piece = Piece.find(params[:id])
     x = piece_params[:x_position].to_i
     y = piece_params[:y_position].to_i
-    if !@piece.valid_move?(x, y)
-      respond_to do |format|
-        format.html {redirect_to game_path(@piece.game), alert: "Invalid move!"}
-      end 
+    return if @piece.valid_move?(x, y)
+
+    respond_to do |format|
+      format.html { flash.alert = 'Invalid move!' }
+      format.json { render json: @piece, status: 422 }
     end
   end
 end
