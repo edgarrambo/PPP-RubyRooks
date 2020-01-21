0c926a26a4f5ce32d2f153431d511e53c234cc42
Shane Quick <shanesfast@gmail.com>
2020-01-20 23:07:56 -0600

    Add specs for pieces#promotion

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -36,4 +36,90 @@ RSpec.describe PiecesController, type: :controller do
       expect(white_queenside_rook.moved).to eq true
     end
   end
+
+  describe 'pieces#promotion' do
+    it 'promotes a White Pawn to a White Queen' do
+      game = create(:game)
+
+      sign_in game.player_one
+
+      white_pawn = create(:pawn, x_position: 7, y_position: 4, piece_number: 5, game_id: game.id)
+      expect(white_pawn.promotable?).to eq true
+
+      put :promotion, params: { piece_id: white_pawn.id, id: white_pawn.id, promotion: 'Queen',
+                                x_position: white_pawn.x_position, y_position: white_pawn.y_position, format: :js }
+
+      promoted_pawn = Piece.find(white_pawn.id)
+
+      expect(promoted_pawn.type).to eq 'Queen'
+      expect(promoted_pawn.id).to eq white_pawn.id
+      expect(promoted_pawn.x_position).to eq 7
+      expect(promoted_pawn.y_position).to eq 4
+      expect(promoted_pawn.piece_number).to eq 3
+      expect(promoted_pawn.valid_move?(0, 4)).to eq true
+    end
+
+    it 'promotes a White Pawn to a White Rook' do
+      game = create(:game)
+
+      sign_in game.player_one
+
+      white_pawn = create(:pawn, x_position: 7, y_position: 4, piece_number: 5, game_id: game.id)
+      expect(white_pawn.promotable?).to eq true
+
+      put :promotion, params: { piece_id: white_pawn.id, id: white_pawn.id, promotion: 'Rook',
+                                x_position: white_pawn.x_position, y_position: white_pawn.y_position, format: :js }
+
+      promoted_pawn = Piece.find(white_pawn.id)
+
+      expect(promoted_pawn.type).to eq 'Rook'
+      expect(promoted_pawn.id).to eq white_pawn.id
+      expect(promoted_pawn.x_position).to eq 7
+      expect(promoted_pawn.y_position).to eq 4
+      expect(promoted_pawn.piece_number).to eq 0
+      expect(promoted_pawn.valid_move?(0, 4)).to eq true
+    end
+
+    it 'promotes a Black Pawn to a Black Knight' do
+      game = create(:game)
+
+      sign_in game.player_one
+
+      black_pawn = create(:pawn, x_position: 0, y_position: 4, piece_number: 11, game_id: game.id)
+      expect(black_pawn.promotable?).to eq true
+
+      put :promotion, params: { piece_id: black_pawn.id, id: black_pawn.id, promotion: 'Knight',
+                                x_position: black_pawn.x_position, y_position: black_pawn.y_position, format: :js }
+
+      promoted_pawn = Piece.find(black_pawn.id)
+
+      expect(promoted_pawn.type).to eq 'Knight'
+      expect(promoted_pawn.id).to eq black_pawn.id
+      expect(promoted_pawn.x_position).to eq 0
+      expect(promoted_pawn.y_position).to eq 4
+      expect(promoted_pawn.piece_number).to eq 7
+      expect(promoted_pawn.valid_move?(1, 6)).to eq true
+    end
+
+    it 'promotes a Black Pawn to a Black Bishop' do
+      game = create(:game)
+
+      sign_in game.player_one
+
+      black_pawn = create(:pawn, x_position: 0, y_position: 4, piece_number: 11, game_id: game.id)
+      expect(black_pawn.promotable?).to eq true
+
+      put :promotion, params: { piece_id: black_pawn.id, id: black_pawn.id, promotion: 'Bishop',
+                                x_position: black_pawn.x_position, y_position: black_pawn.y_position, format: :js }
+
+      promoted_pawn = Piece.find(black_pawn.id)
+
+      expect(promoted_pawn.type).to eq 'Bishop'
+      expect(promoted_pawn.id).to eq black_pawn.id
+      expect(promoted_pawn.x_position).to eq 0
+      expect(promoted_pawn.y_position).to eq 4
+      expect(promoted_pawn.piece_number).to eq 8
+      expect(promoted_pawn.valid_move?(3, 7)).to eq true
+    end
+  end
 end
