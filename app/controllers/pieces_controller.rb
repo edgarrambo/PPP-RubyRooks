3ffd816e44172abaaa3b8507a8283ed3b8b2a6f5
Shane Quick <shanesfast@gmail.com>
2020-01-20 22:16:37 -0600

    Add promotion for Pawns

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,21 +1,13 @@
 class PiecesController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @piece = Piece.find(params[:id])
-  end
-
   def update
-    @piece = Piece.find(params[:id])
-    @game = Game.find(@piece.game_id)
-    x = params[:x_position].to_i
-    y = params[:y_position].to_i
-    flash.now[:alert] = []
+    update_params
 
-    flash.now[:alert] << 'Invalid move!' unless @piece.valid_move?(x, y)
+    flash.now[:alert] << 'Invalid move!' unless @piece.valid_move?(@x, @y)
     flash.now[:alert] << 'Not your piece!' unless current_player_controls_piece?(@piece)
-    check_response = test_check(@piece, x, y)
-    @piece.move_to!(x, y) if flash.now[:alert].empty?
+    check_response = test_check(@piece, @x, @y)
+    @piece.move_to!(@x, @y) if flash.now[:alert].empty?
     flash.now[:alert] << @game.end_turn(check_response, current_user) if check_response
   end
 
@@ -26,10 +18,31 @@ class PiecesController < ApplicationController
     redirect_to game_path(piece.game)
   end
 
+  def promotion
+    update_params
+
+    return unless %w[Bishop Knight Queen Rook].include?(@promotion)
+
+    white_promotions = { 'Bishop' => 2, 'Knight' => 1, 'Queen' => 3, 'Rook' => 0 }
+    black_promotions = { 'Bishop' => 8, 'Knight' => 7, 'Queen' => 9, 'Rook' => 6 }
+
+    @piece.is_white? ? number = white_promotions[@promotion] : number = black_promotions[@promotion]
+    @piece.update(type: @promotion, piece_number: number)
+  end
+
   private
 
   def piece_params
     params.require(:piece).permit(:x_position, :y_position)
+  end
+
+  def update_params
+    @piece = Piece.find(params[:id])
+    @game = Game.find(@piece.game_id)
+    @x = params[:x_position].to_i
+    @y = params[:y_position].to_i
+    @promotion = params[:promotion]
+    flash.now[:alert] = []
   end
 
   def current_player_controls_piece?(piece)
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   root 'games#index'
+
   resources :pieces, only: %i[show update] do
     get :castle
+    put :promotion
   end
 
   resources :games, only: %i[index new create show] do
     get :update_invited_user, :surrender
   end
-
 end
