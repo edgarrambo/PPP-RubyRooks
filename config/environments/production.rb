8b47201df94146a7194be70df5f74246215f7877
Shane Quick <shanesfast@gmail.com>
2020-01-28 17:57:35 -0600

    Prep actioncable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,9 +45,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
+  config.web_socket_server_url = 'wss://peninsula-pirate-pawns.herokuapp.com/cable'
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://peninsula-pirate-pawns.herokuapp.com/', 'http://peninsula-pirate-pawns.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
